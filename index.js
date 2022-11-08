df2bdb43b65fcd24eaed786cfd792de711131d38
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-11-07 23:58:21 -0600

    transformed inquirers consts into functions, completed inquirer prompts and loops

--- index.js
+++ index.js
@@ -2,110 +2,133 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
 
 //define variable for newly created team
 let newTeam = [];
 
-//manager questions array
-const managerQs = [
-    {
-        type: 'input',
-        name: 'teamName',
-        message: 'Please enter a name for your team:',
-    },
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter the team manager\'s name:',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Please enter the team manager\'s employee ID number:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter the team manager\'s email address:',
-    },
-    {
-        type: 'input',
-        name: 'phone',
-        message: 'Please enter your office phone number:',
-    }
-]
+//manager questions array, answers get pushed to team array
+const newManager = () => {
+    return inquirer
+        .prompt([{
+            type: 'input',
+            name: 'teamName',
+            message: 'Please enter a name for your team:',
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the team manager\'s name:',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the team manager\'s employee ID number:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the team manager\'s email address:',
+        },
+        {
+            type: 'input',
+            name: 'phone',
+            message: 'Please enter your office phone number:',
+        }
+        ])
+        .then((data => {
+            const manager = new Manager(data.teamName, data.name, data.id, data.email, data.phone);
+            newTeam.push(manager);
+            console.log(`Welcome to the team, ${data.name}!`);
+            decisionPoint();
+        }))
+}
 
-const decisionPoint = [
-    {
-        type: 'list',
-        name: 'decision',
-        message: 'Select a new job type to add to the team, or select "done" if your team is complete:',
-        choices: ['Engineer', 'Intern', 'Done']
-    }
-]
-
-const engineerQs = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter the engineer\'s name:',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Please enter the engineer\'s employee ID number:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter the engineer\'s email address:',
-    },
-    {
-        type: 'input',
-        name: 'gitHub',
-        message: 'Please enter the engineer\'s GitHub ID:'
-    }
-]
-
-const internQs = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter the intern\'s name:',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Please enter the intern\'s employee ID number:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter the intern\'s email address:',
-    },
-    {
-        type: 'input',
-        name: 'gitHub',
-        message: 'Please enter the intern\'s school:'
-    }
-]
-
-
-//create function to write HTML file
-function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, (err) => {
-            console.log(err ? err : "File successfully written!");
-        })
-    }
-
-//function to initialize app
-function init() {
-        inquirer
-            .prompt(managerQs)
-            .then((data) => {
-                writeToFile(`./dist/${data.teamName}.html`, new Manager(data))
+const decisionPoint = () => {
+    return inquirer
+        .prompt([{
+            type: 'list',
+            name: 'decision',
+            message: 'Select a new job type to add to the team, or select "Done" if your team is complete:',
+            choices: ['Engineer', 'Intern', 'Done']
+        }
+        ])
+        .then((data) => {
+            if (data.decision == 'Engineer') {
+                newEngineer();
+            } else if (data.decision == 'Intern') {
+                newIntern();
+            } else {
+                console.log('Okay, you\'re all done!')
+            }
             })
+        }
+
+//engineer questions array, answers get pushed to team array
+const newEngineer = () => {
+        return inquirer
+            .prompt([{
+                type: 'input',
+                name: 'name',
+                message: 'Please enter the engineer\'s name:',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please enter the engineer\'s employee ID number:',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter the engineer\'s email address:',
+            },
+            {
+                type: 'input',
+                name: 'gitHub',
+                message: 'Please enter the engineer\s GitHub ID:',
+            }
+            ])
+            .then((data => {
+                const engineer = new Engineer(data.name, data.id, data.email, data.gitHub);
+                newTeam.push(engineer);
+                console.log(`Welcome to the team, ${data.name}!`);
+                decisionPoint();
+            }))
     }
-fs.writeFileSync()
-//function call to initialize app
-init();
+
+    //intern questions array, answers get pushed to team array
+    const newIntern = () => {
+        return inquirer
+            .prompt([{
+                type: 'input',
+                name: 'name',
+                message: 'Please enter the intern\'s name:',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please enter the intern\'s employee ID number:',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter the intern\'s email address:',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Please enter the intern\'s school name:',
+            }
+            ])
+            .then((data => {
+                const intern = new Intern(data.name, data.id, data.email, data.school);
+                newTeam.push(intern);
+                console.log(`Welcome to the team, ${data.name}!`);
+                decisionPoint();
+            }))
+    }
+
+    //function call to initialize app
+    newManager()
